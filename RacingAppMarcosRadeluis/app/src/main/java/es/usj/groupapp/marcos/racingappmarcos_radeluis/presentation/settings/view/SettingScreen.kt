9b9dd6a0d89732235b66c992879b0fef2539ac1c
Radeluis Sanchez <rschz.dev@gmail.com>
2025-03-06 00:08:56 +0100

    feat: Agrega navegación, pantallas de base de carreras y configuraciones

---
+++ RacingAppMarcosRadeluis/app/src/main/java/es/usj/groupapp/marcos/racingappmarcos_radeluis/presentation/settings/view/SettingScreen.kt
@@ -0,0 +1,14 @@
+package es.usj.groupapp.marcos.racingappmarcos_radeluis.presentation.settings.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun SettingScreen() {
+    Column(){
+        Text("Settings Screen")
+    }
+
+}
