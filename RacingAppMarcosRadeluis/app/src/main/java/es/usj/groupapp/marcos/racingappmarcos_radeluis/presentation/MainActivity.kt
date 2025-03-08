33787889f6214444a84a01c7244ce56e9b583eed
Radeluis Sanchez <rschz.dev@gmail.com>
2025-03-08 20:42:35 +0100

    feat: Pantalla de configuraciones con opción de activar modo oscuro

--- RacingAppMarcosRadeluis/app/src/main/java/es/usj/groupapp/marcos/racingappmarcos_radeluis/presentation/MainActivity.kt
+++ RacingAppMarcosRadeluis/app/src/main/java/es/usj/groupapp/marcos/racingappmarcos_radeluis/presentation/MainActivity.kt
@@ -12,9 +12,12 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -30,10 +33,10 @@ import es.usj.groupapp.marcos.racingappmarcos_radeluis.data.datasource.local.roo
 import es.usj.groupapp.marcos.racingappmarcos_radeluis.presentation.home.view.HomeScreen
 import es.usj.groupapp.marcos.racingappmarcos_radeluis.presentation.home.viewmodel.HomeViewModel
 import es.usj.groupapp.marcos.racingappmarcos_radeluis.presentation.home.viewmodel.HomeViewModelFactory
-import es.usj.groupapp.marcos.racingappmarcos_radeluis.presentation.races.view.RacesScreen
 import es.usj.groupapp.marcos.racingappmarcos_radeluis.presentation.racers.view.RacerFormScreen
 import es.usj.groupapp.marcos.racingappmarcos_radeluis.presentation.racers.viewmodel.RacerFormViewModel
 import es.usj.groupapp.marcos.racingappmarcos_radeluis.presentation.racers.viewmodel.RacerFormViewModelFactory
+import es.usj.groupapp.marcos.racingappmarcos_radeluis.presentation.races.view.RacesScreen
 import es.usj.groupapp.marcos.racingappmarcos_radeluis.presentation.settings.view.SettingScreen
 import es.usj.groupapp.marcos.racingappmarcos_radeluis.presentation.team.view.TeamFormScreen
 import es.usj.groupapp.marcos.racingappmarcos_radeluis.presentation.team.viewmodel.TeamFormViewModel
@@ -51,66 +54,72 @@ class MainActivity : ComponentActivity() {
             val homeFactory = HomeViewModelFactory(this, database)
             val navController = rememberNavController()
 
-            Scaffold(
-                bottomBar = { BottomNavigation(navController) },
+            var isDarkMode = remember { mutableStateOf(false) }
 
-                ) { innerPadding ->
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(innerPadding),
-                ) {
-                    NavHost(navController = navController, startDestination = "home") {
-                        composable("home") {
-                            val homeViewModel = homeFactory.create(HomeViewModel::class.java)
-                            HomeScreen(viewModel = homeViewModel, navController = navController)
-                        }
+            MaterialTheme(
+                colorScheme = if (isDarkMode.value) darkColorScheme() else lightColorScheme()
+            ) {
+                Scaffold(
+                    bottomBar = { BottomNavigation(navController) },
 
-                        composable("races") {
-                            RacesScreen()
-                        }
+                    ) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                    ) {
+                        NavHost(navController = navController, startDestination = "home") {
+                            composable("home") {
+                                val homeViewModel = homeFactory.create(HomeViewModel::class.java)
+                                HomeScreen(viewModel = homeViewModel, navController = navController)
+                            }
 
-                        composable("settings") {
-                            SettingScreen()
-                        }
+                            composable("races") {
+                                RacesScreen()
+                            }
 
-                        composable("team_form") { backStackEntry ->
-                            val teamFactory = TeamFormViewModelFactory(
-                                context = this@MainActivity,
-                                database = database,
-                                savedStateHandle = backStackEntry.savedStateHandle
-                            )
-                            val teamViewModel = teamFactory.create(TeamFormViewModel::class.java)
+                            composable("settings") {
+                                SettingScreen(isDarkMode)
+                            }
 
-                            TeamFormScreen(viewModel = teamViewModel, navController = navController)
-                        }
+                            composable("team_form") { backStackEntry ->
+                                val teamFactory = TeamFormViewModelFactory(
+                                    context = this@MainActivity,
+                                    database = database,
+                                    savedStateHandle = backStackEntry.savedStateHandle
+                                )
+                                val teamViewModel = teamFactory.create(TeamFormViewModel::class.java)
 
-                        composable("racer_form") { backStackEntry ->
-                            val racerFactory = RacerFormViewModelFactory(
-                                context = this@MainActivity,
-                                database = database,
-                                savedStateHandle = backStackEntry.savedStateHandle
-                            )
-                            val racerViewModel = racerFactory.create(RacerFormViewModel::class.java)
+                                TeamFormScreen(viewModel = teamViewModel, navController = navController)
+                            }
 
-                            RacerFormScreen(
-                                viewModel = racerViewModel,
-                                navController = navController
-                            )
-                        }
+                            composable("racer_form") { backStackEntry ->
+                                val racerFactory = RacerFormViewModelFactory(
+                                    context = this@MainActivity,
+                                    database = database,
+                                    savedStateHandle = backStackEntry.savedStateHandle
+                                )
+                                val racerViewModel = racerFactory.create(RacerFormViewModel::class.java)
 
-                        composable("track_form") { backStackEntry ->
-                            val trackFactory = TrackFormViewModelFactory(
-                                context = this@MainActivity,
-                                database = database,
-                                savedStateHandle = backStackEntry.savedStateHandle
-                            )
-                            val trackViewModel = trackFactory.create(TrackFormViewModel::class.java)
+                                RacerFormScreen(
+                                    viewModel = racerViewModel,
+                                    navController = navController
+                                )
+                            }
 
-                            TrackFormScreen(
-                                viewModel = trackViewModel,
-                                navController = navController
-                            )
+                            composable("track_form") { backStackEntry ->
+                                val trackFactory = TrackFormViewModelFactory(
+                                    context = this@MainActivity,
+                                    database = database,
+                                    savedStateHandle = backStackEntry.savedStateHandle
+                                )
+                                val trackViewModel = trackFactory.create(TrackFormViewModel::class.java)
+
+                                TrackFormScreen(
+                                    viewModel = trackViewModel,
+                                    navController = navController
+                                )
+                            }
                         }
                     }
                 }
